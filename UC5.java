4fd93711d5af87ba44a96add2d9cbf30dd02f6e6
abeerpateria <abeer.pateria0011@gmail.com>
2026-03-11 14:44:44 +0530

    Add OOPSBannerUC6 class with ASCII art methods

---
+++ UC5.java
@@ -0,0 +1,44 @@
+public class OOPSBannerUC6 {
+
+    
+    static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+ 
+    static String[] getP() {
+        return new String[]{
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+   
+    static String[] getS() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*      ",
+            " ***** ",
+            "      *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O = get
